b0cf82b5330f9de0ad4606aedd2169cfa492a796
MOHAN GOWDA <103551008+mohangowdak@users.noreply.github.com>
2023-10-17 14:34:25 +0530

    Update Fireflink-Client.sh

--- Fireflink-Client.sh
+++ Fireflink-Client.sh
@@ -6,6 +6,7 @@ sudo apt-get install xvfb -y
 export DISPLAY=':99.0'
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 sudo apt install libgtk-3-0 libgtk-3-dev -y
+sudo apt install libfuse2 -y
 
 echo "Downloading Client"
 curl -O  "https://testyantrademourl.s3.ap-south-1.amazonaws.com/FireFlinkClient-1.0.12.AppImage"
