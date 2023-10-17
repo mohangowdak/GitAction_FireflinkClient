7acbb8788467d249ae2735cb3dd33b866434aeb4
MOHAN GOWDA <103551008+mohangowdak@users.noreply.github.com>
2023-10-17 13:01:32 +0530

    Add files via upload

---
+++ Fireflink-Client.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+echo "Installing Display"
+sudo apt-get install xvfb -y
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+sudo apt install libgtk-3-0 libgtk-3-dev -y
+
+echo "Downloading Client"
+curl -O  "https://testyantrademourl.s3.ap-south-1.amazonaws.com/FireFlinkClient-1.0.12.AppImage"
+
+chmod 777 FireFlinkClient-1.0.12.AppImage
+./FireFlinkClient-1.0.12.AppImage
+
+
+echo "Fireflink client successfully installed"
+
---
+++ Signin.sh
@@ -0,0 +1,6 @@
+curl --location 'http://127.0.0.1:8111/ci-pipeLine' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "emailId": "manikanth.n@testyantra.com",
+    "password": "Password@123"
+}'
