ff8759f6024f0f0f7ee2c9c34e5b66e66b8eb18a
MOHAN GOWDA <103551008+mohangowdak@users.noreply.github.com>
2023-10-18 07:56:10 +0530

    Update Signin.sh

--- Signin.sh
+++ Signin.sh
@@ -1,6 +1,6 @@
 curl --location 'http://127.0.0.1:8111/ci-pipeLine' \
 --header 'Content-Type: application/json' \
---data-raw '{
-    "emailId": "$1",
-    "password": "$2"
-}'
+--data-raw "{
+    \"emailId\": \"$1\",
+    \"password\": \"$2\"
+}"
